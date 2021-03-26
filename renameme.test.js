f09d53211fc6adc12ccfa65c5c6cae3526581960
Andrew Whitehouse <andrew@whitehouse.org.uk>
2021-03-26 17:47:27 +0000

    Make test fail.

--- renameme.test.js
+++ renameme.test.js
@@ -1,5 +1,7 @@
+const {doSomething} = require('./renameme')
+
 describe('number converter', () => {
-  xit('Should convert 1 to one', () => {
-    expect(say(1)).toBe("one");
+  it('Should convert 1 to one', () => {
+    expect(doSomething(1)).toBe(0);
   });
 });
